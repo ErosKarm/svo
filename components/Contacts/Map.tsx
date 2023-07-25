0cec67eac9f1aa6031155421a97498685d356acb
ErosKarm <eroskarm@gmail.com>
2023-07-25 16:22:31 +0300

    window deployment fix

--- components/Contacts/Map.tsx
+++ components/Contacts/Map.tsx
@@ -11,23 +11,27 @@ import L from "leaflet";
 const icon = L.icon({ iconUrl: "./assets/icons/marker-icon.png" });
 
 const Map = () => {
-  return (
-    <MapContainer
-      className={styles.map}
-      center={[56.981, 24.118]}
-      zoom={13}
-      scrollWheelZoom={false}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-        url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
-      />
+  if (typeof window !== "undefined") {
+    return (
+      <MapContainer
+        className={styles.map}
+        center={[56.981, 24.118]}
+        zoom={13}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+          url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+        />
 
-      <Marker position={[56.981, 24.118]} icon={icon}>
-        <Popup>Ganibu dambis 24D, Riga, LV-1005, Latvia</Popup>
-      </Marker>
-    </MapContainer>
-  );
+        <Marker position={[56.981, 24.118]} icon={icon}>
+          <Popup>Ganibu dambis 24D, Riga, LV-1005, Latvia</Popup>
+        </Marker>
+      </MapContainer>
+    );
+  }
+
+  return <></>;
 };
 
 export default Map;
